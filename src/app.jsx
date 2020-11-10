122bcf9da05364a437cadeb3bb5253b418df7727
JOON HYUK LEE <joonhyuklee@MacBook-Pro-2.local>
2020-11-10 21:19:06 +0900

    show video list with title //youtube API

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello :)</h1>;
+
+    const [videos, setVideos] = useState([]);//useState Api 이용 setvideo는 함수 
+    
+    useEffect(()=>{ 
+      const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBgi4KNI7kLRVfi7sBphsRmX64RXXtm6AY", requestOptions)
+  .then(response => response.json())
+  .then(result => setVideos(result.items))
+  .catch(error => console.log('error', error));
+    }, []);//컴포넌트가 업데이트 될때 마다 네트워크 통신을 하면 안좋으므로 텅빈 배열을[] 2번째인자로 전달하면 마운트가 될때만 이부분이호출
+  return <VideoList videos={videos}/>
 }
 
 export default App;
