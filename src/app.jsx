65c3436d7f9ba58689c6ab66c0aa70f9f7f926bf
JOON HYUK LEE <joonhyuklee@MacBook-Pro-2.local>
2020-11-12 23:17:32 +0900

    youtube service Code Refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -4,31 +4,17 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {
     const [videos, setVideos] = useState([]);//useState Api 이용 setvideo는 함수 
     const search = query =>{
-      const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
+      youtube
+        .search(query)//프리티어 자동 방자
+        .then(videos => setVideos(videos));//promise 가 return이 되면 아이탬들이 리런
       
-      fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBgi4KNI7kLRVfi7sBphsRmX64RXXtm6AY`, requestOptions)
-        .then(response => response.json())
-        .then(result => result.items.map(
-          item => ({...item , id: item.id.videoId}))) //id를 받아서 새로운 오브젝트를 생성 기존정보를유지하고 id만 오브젝트가 아닌 video id 로 덮어줌  
-        .then(items => setVideos(items))
-        .catch(error => console.log('error', error));
-    }
+    };
     useEffect(()=>{ 
-      const requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
-
-fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBgi4KNI7kLRVfi7sBphsRmX64RXXtm6AY", requestOptions)
-  .then(response => response.json())
-  .then(result => setVideos(result.items))
-  .catch(error => console.log('error', error));
+      youtube.mostPopular()//
+      .then(videos => setVideos(videos));
     }, []);//컴포넌트가 업데이트 될때 마다 네트워크 통신을 하면 안좋으므로 텅빈 배열을[] 2번째인자로 전달하면 마운트가 될때만 이부분이호출
   return (
     <div className = {styles.app}>
